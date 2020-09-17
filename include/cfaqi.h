4fd9f85ff7064b2edc63dce8e0d2788228e98c91
Lorenzo Flueckiger <Lorenzo.Flueckiger@gmail.com>
2020-09-16 21:30:34 -0700

    Added conversion from concentration to aqi and UT

---
+++ include/cfaqi.h
@@ -0,0 +1,60 @@
+#include <stdint.h>
+#include <math.h>
+
+const int AQI_LEVELS = 6;
+
+enum class AqiLevel : int8_t {
+  OutOfRange = -1,
+  Good = 0,
+  Moderate = 1,
+  USG = 2,
+  Unhealthy = 3,
+  VeryUnhealthy = 4,
+  Hazardous = 5
+};
+
+const char *AqiNames[AQI_LEVELS] = {
+    "Good",      "Moderate",       "Unhealthy For Sensitive Groups",
+    "Unhealthy", "Very Unhealthy", "Hazardous"};
+
+const char *AqiColors[AQI_LEVELS] = {"Green", "Yellow", "Orange",
+                                     "Red",   "Purple", "Marron"};
+
+// From page 11 of:
+// https://www.airnow.gov/sites/default/files/2018-05/aqi-technical-assistance-document-may2016.pdf
+
+const float ConcentrationBreakpoints[AQI_LEVELS + 2] = {
+    -0.1f, 12.0f, 35.4f, 55.4f, 150.4f, 250.4f, 350.4f, 500.4f};
+
+const int16_t AqiBreakpoints[AQI_LEVELS + 2] = {-1,   50,  100, 150,
+                                                200, 300, 400, 500};
+
+bool pm25_to_aqi(float pm, int16_t &aqiValue, AqiLevel &aqiLevel) {
+  bool valid = false;
+  aqiValue = -1;
+  aqiLevel = AqiLevel::OutOfRange;
+  int bracket = 0;
+  if (pm >= 0.0) {
+    for (int i = 1; i < AQI_LEVELS + 2; i++) {
+      if (pm <= ConcentrationBreakpoints[i]) {
+        valid = true;
+        bracket = i;
+        break;
+      }
+    }
+  }
+  if (pm > ConcentrationBreakpoints[AQI_LEVELS+1] ) {
+      // case completely out of range -> we saturate the value 
+      aqiValue = 500;
+  }
+  if (valid) {
+    aqiLevel = static_cast<AqiLevel>(bracket-1);
+    int lowI = AqiBreakpoints[bracket-1]+1;
+    int highI = AqiBreakpoints[bracket];
+    float lowC = ConcentrationBreakpoints[bracket-1]+0.1f;
+    float highC = ConcentrationBreakpoints[bracket];
+    aqiValue = roundf( lowI + (highI-lowI)/(highC-lowC)*(pm-lowC) );
+  }
+
+  return valid;
+}
---
+++ test/test_cfaqi/test_cfaqi.cpp
@@ -0,0 +1,50 @@
+#include "cfaqi.h"
+#include "unity.h"
+
+void test_function_pm25_to_aqi_out_of_range(void) {
+  int16_t value;
+  AqiLevel level;
+  bool valid;
+
+  valid = pm25_to_aqi(-10.0, value, level);
+  TEST_ASSERT_FALSE(valid);
+  TEST_ASSERT_EQUAL(value, -1);
+  TEST_ASSERT_EQUAL(level, AqiLevel::OutOfRange);
+  valid = pm25_to_aqi(600.0, value, level);
+  TEST_ASSERT_FALSE(valid);
+  TEST_ASSERT_EQUAL(value, 500);
+  TEST_ASSERT_EQUAL(level, AqiLevel::OutOfRange);
+}
+
+void test_function_pm25_to_aqi_conversions(void) {
+  const float concentrations[] = {0.0,   5.0,   10.0,  12.0,  12.1,  15.0,
+                                  35.4,  35.5,  40.0,  55.4,  55.5,  100.0,
+                                  150.4, 150.5, 200.0, 250.4, 250.5, 300.0,
+                                  350.4, 350.5, 400.0, 500.4};
+  // conversion from: https://www.airnow.gov/aqi/aqi-calculator-concentration/
+  const int aqivalues[] = {0, 21,  42,  50,  51,  57,  100, 101,
+                           112, 150, 151, 174, 200, 201, 250, 300,
+                           301, 350, 400, 401, 434, 500};
+  const int aqilevels[] = {0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3,
+                           3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6};
+
+  int i = 0;
+  for (float c : concentrations) {
+    int16_t value;
+    AqiLevel level;
+    bool valid = pm25_to_aqi(c, value, level);
+    TEST_ASSERT_TRUE(valid);
+    TEST_ASSERT_EQUAL(aqivalues[i], value);
+    TEST_ASSERT_EQUAL(static_cast<int>(level), level);
+    i++;
+  }
+}
+
+int main(int argc, char **argv) {
+  UNITY_BEGIN();
+  RUN_TEST(test_function_pm25_to_aqi_out_of_range);
+  RUN_TEST(test_function_pm25_to_aqi_conversions);
+  UNITY_END();
+
+  return 0;
+}
