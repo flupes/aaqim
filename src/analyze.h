369133754a911fc150a3048a93f41534ed24480f
Lorenzo Flueckiger <Lorenzo.Flueckiger@gmail.com>
2020-09-20 17:52:36 -0700

    working prototype :-)

---
+++ src/analyze.h
@@ -0,0 +1,64 @@
+#ifndef AAQIM_ANALYZE_H
+#define AAQIM_ANALYZE_H
+
+#if defined(ARDUINO)
+
+#include <stdlib.h>
+
+#include "air_sample.h"
+#include "sensors.h"
+#include "stats.h"
+
+static const uint32_t kMaxReadingAgeSeconds = 12 * 60;
+static const float kMaxPercentDiscrepancy = 0.08;
+
+size_t ComputeStats(const AirSensors& sensors, AirSample& sample) {
+  short primary = -1;
+  size_t count = 0;
+  float cfvalues[2 * kMaxSensors];
+
+  for (size_t i = 0; i < sensors.Count(); i++) {
+    const SensorData& data = sensors.Data(i);
+
+    // check age
+    if (data.age_A > 5 || data.age_B > 5 || abs(data.age_A - data.age_B) > 15) {
+      break;
+    }
+
+    // check sensors consistency
+    if (fabs(data.pm_2_5_A - data.pm_2_5_B) / (data.pm_2_5_A + data.pm_2_5_B) >
+        kMaxPercentDiscrepancy) {
+      break;
+    }
+
+    // keep which sensor is considered the primary one
+    if (primary < 0) {
+      primary = i;
+    }
+
+    cfvalues[count * 2] = data.pm_2_5_A;
+    cfvalues[count * 2 + 1] = data.pm_2_5_B;
+    count++;
+  }
+
+  if (primary < 0) {
+    Serial.println("Could not identify a primary sensor!");
+    sample.Set(0, 0.0f, 0.0f, 0.0f, 0.0f, 0, 0, 0, 0);
+  } else {
+    Serial.print("Compute stats with n sensors = ");
+    Serial.println(count);
+    float avg, nmae;
+    mean_error(count * 2, cfvalues, avg, nmae);
+    Serial.print("NMAE = ");
+    Serial.println(nmae);
+    const SensorData& data = sensors.Data(primary);
+    sample.Set(data.timestamp, 0.0, avg, 0.0, data.pressure, data.temperature,
+               data.humidity, count, nmae);
+  }
+
+  return count;
+}
+
+#endif
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,141 +1,165 @@
 #if defined(ARDUINO)
 
+#include <Adafruit_GFX.h>
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-// #include <StreamUtils.h>
+#include <time.h>
 
-#include "credentials.h"
-#include "stats.h"
+#include "Fonts/ClearSans-Bold-48pt7b.h"
+#include "Fonts/ClearSans-Medium-12pt7b.h"
+#include "Fonts/ClearSans-Medium-18pt7b.h"
+#include "air_sample.h"
+#include "analyze.h"
 #include "cfaqi.h"
+#include "credentials.h"
+#include "epd2in7b.h"
+#include "sensors.h"
 
-// how to:
-// https://arduinojson.org/v6/how-to/use-arduinojson-with-httpclient/
+#define COLORED 1
+#define UNCOLORED 0
 
-ESP8266WiFiMulti wifiMulti;
+Epd epd;
+GFXcanvas1 *canvas[2];
 
-String request_str =
-    "http://www.purpleair.com/json?show=59927|65489|54857|36667|25301";
-// String request_str = "http://www.purpleair.com/json?show=59927";
+const time_t kTimeZoneOffsetSeconds = -7 * 3600;
+
+enum class ColorBuffer : uint8_t { Black = 0, Red = 1 };
+
+void CenterText(const GFXfont *font, const char *str, uint16_t line,
+                ColorBuffer color = ColorBuffer::Black) {
+  int16_t x, y;
+  uint16_t w, h;
+  uint8_t bufferId = static_cast<uint8_t>(color);
+  canvas[bufferId]->setFont(font);
+  canvas[bufferId]->getTextBounds(str, 0, 100, &x, &y, &w, &h);
+  int16_t offset = 0;
+  if ( str[0] == '1' ) {
+    offset = h/8;
+  }
+  canvas[bufferId]->setCursor((EPD_WIDTH - w) / 2 - offset, line);
+  canvas[bufferId]->print(str);
+}
 
 void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
   Serial.println();
 
-  wifiMulti.addAP(SSID, PASS);
-}
+  Serial.println("Init e-Paper...");
+  if (epd.Init() != 0) {
+    Serial.println("e-Paper init failed");
+    return;
+  }
+  epd.ClearFrame();
 
-void loop() {
-  static bool connectionWasAlive = true;
+  canvas[0] = new GFXcanvas1(EPD_WIDTH, EPD_HEIGHT);
+  canvas[1] = new GFXcanvas1(EPD_WIDTH, EPD_HEIGHT);
 
-  if (wifiMulti.run() != WL_CONNECTED) {
-    if (connectionWasAlive == true) {
-      connectionWasAlive = false;
-      Serial.print("Looking for WiFi ");
-    }
-    Serial.print(".");
+  for (uint8_t i = 0; i < 2; i++) {
+    canvas[i]->fillScreen(UNCOLORED);
+    canvas[i]->setTextColor(COLORED);
+    canvas[i]->setTextSize(1);
+    canvas[i]->setTextWrap(false);
+  }
+
+  Serial.print("Looking for wifi ");
+  unsigned long start = millis();
+  bool timeout = false;
+  WiFi.begin(SSID, PASS);
+  while (!timeout && WiFi.status() != WL_CONNECTED) {
     delay(500);
-  } else {
-    if (connectionWasAlive == false) {
-      Serial.println("Connected to WiFi :-)");
+    Serial.print(".");
+    if (millis() - start > 45 * 1000) {
+      timeout = true;
     }
-
+  }
+  Serial.println();
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println();
+    Serial.println("WiFi Connected :-)");
     WiFiClient client;
     HTTPClient http;
 
-    // Send request
-    http.useHTTP10(true);
-    http.begin(client, request_str);
-    http.GET();
-
-    // Parse response
-    DynamicJsonDocument doc(16684);
-
-    auto err = deserializeJson(doc, http.getStream());
-    // ReadLoggingStream loggingStream(http.getStream(), Serial);
-    // deserializeJson(doc, loggingStream);
-
-    // Parse succeeded?
-    if (err) {
-      Serial.print(F("deserializeJson() returned "));
-      Serial.println(err.c_str());
-      return;
+    AirSensors sensors;
+    for (size_t i = 0; i < sizeof(kSensorIds) / sizeof(size_t); i++) {
+      sensors.AddSensor(kSensorIds[i]);
     }
-
-    auto memoryUsed = doc.memoryUsage();
-    Serial.print("Document Memory Usage = ");
-    Serial.println(memoryUsed);
-
-    auto version = doc["mapVersion"].as<String>();
-    Serial.print("mapVersion = ");
-    Serial.println(version);
-
-    JsonArray sensorsArray = doc["results"].as<JsonArray>();
-    Serial.print("Array count = ");
-    Serial.println(sensorsArray.size());
-
-    const int MAX_READINGS = 32;
-    float cfvalues[MAX_READINGS];
-    int readingsCount = 0;
-
-    for (JsonObject sensor : sensorsArray) {
-      int32_t id = sensor["ID"];
-      Serial.print("ID = ");
-      Serial.print(id);
-      String label = sensor["Label"];
-      Serial.print(" / Label = ");
-      Serial.println(label);
-      int32_t parent = sensor["ParentID"];
-      if (parent == 0) {
-        // This is the primary sensor (no parent)
-        int16_t temperature_f = sensor["temp_f"];
-        Serial.print("  Temperature deg F = ");
-        Serial.println(temperature_f);
-        int16_t humidity = sensor["humidity"];
-        Serial.print("  Humidity % = ");
-        Serial.println(humidity);
-        float pressure = sensor["pressure"];
-        Serial.print("  Pressure mb = ");
-        Serial.println(pressure);
-      } else {
-        Serial.print("  parentID = ");
-        Serial.println(parent);
-      }
-      float pm25cf = sensor["pm2_5_cf_1"];
-      if (readingsCount < MAX_READINGS - 1) {
-        cfvalues[readingsCount++] = pm25cf;
-      }
-      Serial.print("  pm 2.5 cf = ");
-      Serial.println(pm25cf);
-    }
+    Serial.println("Update data");
+    sensors.UpdateData(client, http);
+    Serial.println("List of sensors");
+    sensors.PrintAllData();
 
     // Disconnect
     http.end();
 
-    Serial.print("Number of readings = ");
-    Serial.println(readingsCount);
-    float avg;
-    float error;
-    mean_error(readingsCount, cfvalues, avg, error);
-    Serial.print("average PM2.5 = ");
-    Serial.print(avg);
-    Serial.print(" / error = ");
-    Serial.println(error);
+    AirSample sample;
+    size_t nbSamples = ComputeStats(sensors, sample);
+    if (nbSamples > 0) {
+      time_t seconds = sample.Seconds() + kTimeZoneOffsetSeconds;
+      tm *local = gmtime(&seconds);
+      char date[16];
+      strftime(date, 16, "%b %d, %Y", local);
+      char stamp[16];
+      strftime(stamp, 16, "%H:%M:%S", local);
+      Serial.print(date);
+      Serial.print(" - ");
+      Serial.println(stamp);
+      char datetime[16];
+      strftime(datetime, 16, "%b %d, %H:%M", local);
 
-    int16_t aqiValue;
-    AqiLevel aqiLevel;
-    pm25_to_aqi(avg, aqiValue, aqiLevel);
-    Serial.print("AQI = ");
-    Serial.print(aqiValue);
-    Serial.print(" --> ");
-    Serial.println(AqiNames[static_cast<int>(aqiLevel)]);
+      CenterText(&ClearSans_Medium12pt7b, datetime, 24);
+      
+      int16_t aqi = sample.AqiPm_2_5(); 
+      Serial.print("AQI = ");
+      Serial.print(aqi);
+      Serial.print(" --> ");
+      Serial.println(AqiNames[static_cast<int>(sample.Level())]);
+      if ( aqi > 0.1 ) {
+        canvas[1]->fillRoundRect(7, 36, EPD_WIDTH-2*7, 68, 8, COLORED);
+        canvas[1]->fillRoundRect(10, 39, EPD_WIDTH-2*10, 62, 4, UNCOLORED);
+      }
+      String aqiValue(aqi);
+      CenterText(&ClearSans_Bold48pt7b, aqiValue.c_str(), 96);
 
-    delay(60 * 1000);
+      String aqiName(AqiNames[static_cast<int>(sample.Level())]);
+      CenterText(&ClearSans_Medium18pt7b, aqiName.c_str(), 132);
+
+      String count = String("# ") + sample.SamplesCount() + " sensors";
+      CenterText(&ClearSans_Medium12pt7b, count.c_str(), 160);
+
+      float nmae = sample.NmaeValue();
+      if ( nmae > 0.1 ) {
+        canvas[1]->fillRoundRect(34, 164, EPD_WIDTH-2*34, 24, 6, COLORED);
+        canvas[1]->fillRoundRect(36, 166, EPD_WIDTH-2*36, 20, 4, UNCOLORED);
+      }
+      String stats = String("err=") +
+                     String(nmae * 100.0f, 0) + String("%");
+      CenterText(&ClearSans_Medium12pt7b, stats.c_str(), 182);
+
+    } else {
+      Serial.println("No valid air sample retrieved :-(");
+    }
+
+  } else {
+    // not connected, too bad :-(
+    Serial.println("Could not connected to WiFi :-(");
   }
+
+  epd.TransmitPartial(canvas[0]->getBuffer(), canvas[1]->getBuffer(), 0, 0,
+                      EPD_WIDTH, EPD_HEIGHT);
+  epd.DisplayFrame();
+
+  Serial.println("Put display to sleep");
+  epd.Sleep();
+  delay(500);
+
+  Serial.println("Now go to sleep for 4 minutes");
+  ESP.deepSleep(4 * 60 * 1E6);
 }
+
+void loop() {}
 
 #else
 // Something trivial to avoid pio errors
