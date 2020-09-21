a7fc44d34d53645c139d822f1991a6ce81f71684
Lorenzo Flueckiger <Lorenzo.Flueckiger@gmail.com>
2020-09-20 17:50:54 -0700

    cleanup

--- lib/air_sensors/sensors.h
+++ lib/air_sensors/sensors.h
@@ -7,10 +7,7 @@
 class WiFiClient;
 class HTTPClient;
 
-static const size_t kMaxSensors = 8;
-
-static const uint32_t kMaxReadingAgeSeconds = 12 * 60;
-static const float kMaxPercentDiscrepancy = 0.06;
+const size_t kMaxSensors = 8;
 
 // Purple air sensors ID by priority.
 static const size_t kSensorIds[] = {59927, 65489, 67415, 25301, 54857, 36667};
@@ -60,7 +57,7 @@ class AirSensors {
     }
   }
 
-  size_t Count() { return sensorsCount_; }
+  size_t Count() const { return sensorsCount_; }
 
  protected:
   size_t GetSensorIndex(size_t sid) {
@@ -82,49 +79,4 @@ class AirSensors {
   SensorData sensorsData_[kMaxSensors];
 };
 
-#endif
-
-#if 0
-      // For the sensor to be accepted as primary source,
-      // it needs to:
-      //   1) have been updated rencently and
-      //   2) have consistency between the two embdded sensors
-      if (now > 0) {
-        // we have a valid timestamp to compare against
-        if (abs(now - ts1) > kMaxReadingAgeSeconds ||
-            abs(now - ts2) > kMaxReadingAgeSeconds) {
-          return data;
-        }
-      } else {
-        if (age1 > kMaxReadingAgeSeconds / 60 ||
-            age2 > kMaxReadingAgeSeconds / 60) {
-          return data;
-        }
-      }
-      data.timestamp = ts1;
-      data.pm_2_5_A = sensorsArray[0]["pm2_5_cf_1"];
-      data.pm_2_5_B = sensorsArray[1]["pm2_5_cf_1"];
-      if (fabs(data.pm_2_5_A - data.pm_2_5_B) /
-              (data.pm_2_5_A + data.pm_2_5_B) >
-          kMaxPercentDiscrepancy) {
-        return data;
-      }
-      size_t index = 0;
-      int32_t parent = sensorsArray[0]["ParentID"];
-      if (parent != 0) {
-        // it seems that the secondary came first
-        index = 1;
-      }
-      data.temperature = sensorsArray[index]["temp_f"];
-      data.humidity = sensorsArray[index]["humidity"];
-      data.pressure = sensorsArray[index]["pressure"];
-      data.id = sensorsArray[index]["ID"];
-
-      if (data.id != 0) {
-        if (primarySensorId_ == 0) {
-          primarySensorId_ = data.id;
-          lastTimestamp_ = data.timestamp;
-        }
-        sensorsData_.push_back(data);
-      }
 #endif
