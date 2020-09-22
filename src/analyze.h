f9cc15ca13aece9f5da65e4ffc0cf8da7748c82f
Lorenzo Flueckiger <Lorenzo.Flueckiger@gmail.com>
2020-09-21 21:39:14 -0700

    returns the primary sensor used

--- src/analyze.h
+++ src/analyze.h
@@ -10,16 +10,17 @@
 #include "stats.h"
 
 // PA sensors seem to be updated every two minutes.
-// We consider the sensor valid even if we miss two beats
-static const uint32_t kMaxReadingAgeMinutes = 5;
+// We consider the sensor valid even if we miss three beats
+const int16_t kMaxReadingAgeMinutes = 7;
 
 // PA report 100% confidence when the two readings of one station
-// are within 6%. So we should still accept these, but will 
+// are within 6%. So we should still accept these, but will
 // discard the sensor if higher (not sure at what point PA confidence decreases)
-static const float kMaxPercentDiscrepancy = 0.06;
+const float kMaxPercentDiscrepancy = 0.06;
 
-size_t ComputeStats(const AirSensors& sensors, AirSample& sample) {
-  short primary = -1;
+size_t ComputeStats(const AirSensors& sensors, AirSample& sample,
+                    int32_t& primaryIndex) {
+  primaryIndex = -1;
   size_t count = 0;
   float cfvalues[2 * kMaxSensors];
 
@@ -27,19 +28,21 @@ size_t ComputeStats(const AirSensors& sensors, AirSample& sample) {
     const SensorData& data = sensors.Data(i);
 
     // check age
-    if (data.age_A > 5 || data.age_B > 5 || abs(data.age_A - data.age_B) > 15) {
+    if (data.age_A > kMaxReadingAgeMinutes || data.age_B > kMaxReadingAgeMinutes) {
       continue;
     }
 
-    // check sensors consistency
-    if (fabs(data.pm_2_5_A - data.pm_2_5_B) / (data.pm_2_5_A + data.pm_2_5_B) >
-        kMaxPercentDiscrepancy) {
+    // check sensors consistency in percent (it seems that for low readings,
+    // a 1.2 absolute diffence is still acceptable?)
+    float diff = fabs(data.pm_2_5_A - data.pm_2_5_B);
+    if ( (diff / (data.pm_2_5_A + data.pm_2_5_B) >
+        kMaxPercentDiscrepancy) && diff > 1.2) {
       continue;
     }
 
     // keep which sensor is considered the primary one
-    if (primary < 0) {
-      primary = i;
+    if (primaryIndex < 0) {
+      primaryIndex = i;
     }
 
     cfvalues[count * 2] = data.pm_2_5_A;
@@ -47,19 +50,33 @@ size_t ComputeStats(const AirSensors& sensors, AirSample& sample) {
     count++;
   }
 
-  if (primary < 0) {
+  if (primaryIndex < 0) {
     Serial.println("Could not identify a primary sensor!");
     sample.Set(0, 0.0f, 0.0f, 0.0f, 0.0f, 0, 0, 0, 0);
   } else {
     Serial.print("Compute stats with n sensors = ");
     Serial.println(count);
-    float avg, nmae;
-    mean_error(count * 2, cfvalues, avg, nmae);
+    float avgRealTime, nmaeRealTime;
+    mean_error(count * 2, cfvalues, avgRealTime, nmaeRealTime);
+    Serial.print("real time avg = ");
+    Serial.println(avgRealTime);
     Serial.print("NMAE = ");
-    Serial.println(nmae);
-    const SensorData& data = sensors.Data(primary);
-    sample.Set(data.timestamp, 0.0, avg, 0.0, data.pressure, data.temperature,
-               data.humidity, count, nmae);
+    Serial.println(nmaeRealTime);
+
+#if 0
+    float avg10values[kMaxSensors];
+    // Get out strong outliers
+    count = 0;
+    for (size_t i = 0; i < sensors.Count(); i++) {
+      const SensorData& data = sensors.Data(i);
+      float avg10min = data.averages[static_cast<int>(PmAvgIndexes::TenMinutes)];
+      if ( fabs(avg10min-avgRealTime) < 5.0 * nmae*avg10min) {
+        count++;
+      }
+#endif
+    const SensorData& data = sensors.Data(primaryIndex);
+    sample.Set(data.timestamp, 0.0, avgRealTime, 0.0, data.pressure, data.temperature,
+               data.humidity, count, nmaeRealTime);
   }
 
   return count;
