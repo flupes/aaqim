f77ae215befa8b69db05912f3734873a1c553e88
Lorenzo Flueckiger <Lorenzo.Flueckiger@gmail.com>
2020-10-03 23:02:47 -0700

    added class to put samples in display buckets

---
+++ include/display_samples.h
@@ -0,0 +1,134 @@
+#ifndef AAQIM_DISPLAY_SAMPLES_H
+#define AAQIM_DISPLAY_SAMPLES_H
+
+#include "air_sample.h"
+#include "flash_samples.h"
+
+typedef FlashSamples<AirSampleData> FlashAirDataSamples;
+
+template <size_t BUFFER_LENGTH>
+class DisplaySamples {
+ public:
+  DisplaySamples(uint32_t periodInSeconds)
+      : length_(BUFFER_LENGTH), period_(periodInSeconds) {}
+
+  size_t Fill(FlashAirDataSamples &src, uint32_t now) {
+    // Initialize min/max (cannot use INT16_MIN because it is already used
+    // to mark "no data"
+    min_ = INT16_MAX - 1;
+    max_ = INT16_MIN + 1;
+
+    uint32_t count = 0;
+    if (!src.IsScanned() || src.IsEmpty()) {
+      return count;
+    }
+
+    const size_t numberOfAvailableSamples =
+        src.NumberOfSamples();  // this method is not efficient, so cache the
+                                // value
+    size_t samplesIndex = 0;
+    size_t previousSampleIndex = UINT32_MAX;
+    size_t bufferReversedIndex = 0;
+    size_t bucketCount = 0;
+    int32_t accumulator = 0;
+
+    printf("==== now = %d\n", now);
+    AirSampleData data;
+    AirSample sample;
+    while (bufferReversedIndex < length_ &&
+           samplesIndex < numberOfAvailableSamples) {
+      if (previousSampleIndex != samplesIndex) {
+        src.ReadSample(samplesIndex, data);
+        sample.FromData(data);
+        previousSampleIndex = samplesIndex;
+      }
+      uint32_t sampleTimestamp = sample.Seconds();
+      uint32_t bufferMaxTimestamp = now - bufferReversedIndex * period_;
+      uint32_t bufferMinTimestamp = bufferMaxTimestamp - period_;
+
+      printf("samplesIndex =        %ld\n", samplesIndex);
+      printf("sampleTimestamp =     %d (age = %d)\n", sampleTimestamp,
+             now - sampleTimestamp);
+      printf("bufferReversedIndex = %ld\n", bufferReversedIndex);
+      printf("buffer min/max ts =  [%d, %d] | age = (%d, %d)\n",
+             bufferMinTimestamp, bufferMaxTimestamp, now - bufferMaxTimestamp,
+             now - bufferMinTimestamp);
+      printf("bucketCount =         %ld | count = %d\n", bucketCount, count);
+
+      if (bufferMinTimestamp < sampleTimestamp &&
+          sampleTimestamp <= bufferMaxTimestamp) {
+        printf("-- accumulate\n");
+        accumulator += sample.AqiPm_2_5();
+        bucketCount++;
+        samplesIndex++;
+      } else {
+        if (sampleTimestamp <= bufferMinTimestamp) {
+          // move to previous buffer slot
+          if (bucketCount > 0) {
+            printf("-- add sample\n");
+            buffer_[length_ - bufferReversedIndex - 1] =
+                accumulator / bucketCount;
+            count++;
+            accumulator = 0;
+            bucketCount = 0;
+          } else {
+            // No samples belong to this time slice
+            printf("-- mark no data\n");
+            buffer_[length_ - bufferReversedIndex - 1] = INT16_MIN;
+          }
+          bufferReversedIndex++;
+        }
+        if (sampleTimestamp > bufferMaxTimestamp) {
+          // skip sample
+          printf("-- skip sample\n");
+          samplesIndex++;
+        }
+      }
+    }
+    for (size_t r = bufferReversedIndex; r < length_; r++) {
+      printf("## mark %ld with no data\n", r);
+      buffer_[length_ - r - 1] = INT16_MIN;
+    }
+    return count;
+  }
+
+ protected:
+  size_t length_;
+  uint32_t period_;
+  int16_t buffer_[BUFFER_LENGTH];
+  int16_t min_;
+  int16_t max_;
+};
+
+#endif
+
+#if 0
+      if (sampleTimestamp < bufferMinTimestamp) {
+        // move to previous buffer slot
+        if (bucketCount > 0) {
+          printf("-- add sample\n");
+          buffer_[length_ - bufferReversedIndex - 1] =
+              accumulator / bucketCount;
+          count++;
+          accumulator = 0;
+          bucketCount = 0;
+        } else {
+          // No samples belong to this time slice
+          printf("-- mark no data\n");
+          buffer_[length_ - bufferReversedIndex - 1] = INT16_MIN;
+        }
+        bufferReversedIndex++;
+      } else {
+        if (sampleTimestamp > bufferMaxTimestamp) {
+          // skip sample
+          printf("-- skip sample\n");
+          samplesIndex++;
+        } else {
+          printf("-- accumulate\n");
+          accumulator += sample.AqiPm_2_5();
+          bucketCount++;
+          samplesIndex++;
+        }
+      }
+    }
+#endif
---
+++ test/test_display_samples/test_display_samples.cpp
@@ -0,0 +1,73 @@
+#include "display_samples.h"
+#include "unity.h"
+
+#if defined(ARDUINO)
+EspFlash gFlash;
+#else
+#include "sim_flash.h"
+SimFlash gFlash;
+#endif
+
+const uint32_t kFlashOffset = 0x000A0000;
+const uint32_t kNowSeconds = k2019epoch + 365 * 24 * 3600;
+
+FlashAirDataSamples gFlashSamples(gFlash, 64, kFlashOffset);
+
+void TestFillFromEmptyFlash() {
+  gFlashSamples.Begin(true);
+  DisplaySamples<8> displaySamples(300);
+  size_t count = displaySamples.Fill(gFlashSamples, kNowSeconds);
+  TEST_ASSERT_EQUAL(0, count);
+}
+
+void StoreSample(uint32_t age, float pm25) {
+  static uint32_t counter = 1;
+  printf("store sample # %d with age %d --> ts = %d\n", counter++, age, kNowSeconds-age);
+
+  AirSample sample(kNowSeconds - age, 0.0f, pm25, 0.0f, 1000.0f, 77, 33, 3,
+                   0.5f);
+  AirSampleData data;
+  sample.ToData(data);
+  gFlashSamples.StoreSample(data);
+}
+
+void StoreSerie() {
+  // The target samples are designed to results in the current
+  // display buffer (number indicates how many samples is this time slice)
+  //
+  // | 0 | 3 | 1 | 2 | 0 | 0 | 2 | 0 |
+  //
+  // - missing very recent and very old samples
+  // - gap in the middle of the data
+  // - uneven distribution of the samples
+  StoreSample(2050, 110.0f);
+  // sample unordered: should not matter IF within a timeslice!
+  StoreSample(1900, 100.0f);
+  StoreSample(2000, 90.0f);
+
+  StoreSample(1600, 200.0f);
+
+  StoreSample(1400, 250.0f);
+  StoreSample(1300, 350.0f);
+
+  StoreSample(500, 350.0f);
+  StoreSample(400, 450.0f);
+}
+
+void TestFillDisplaySample() {
+  // First we need to populate the flash...
+  StoreSerie();
+
+  DisplaySamples<8> displaySamples(300);
+  size_t count = displaySamples.Fill(gFlashSamples, kNowSeconds);
+  TEST_ASSERT_EQUAL(4, count);
+}
+
+int main() {
+
+  UNITY_BEGIN();
+  RUN_TEST(TestFillFromEmptyFlash);
+  RUN_TEST(TestFillDisplaySample);
+
+  UNITY_END();
+}
