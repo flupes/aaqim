1324d61f0c32d5d6aaeb6f4a74d34f8f0ce2c4eb
Lorenzo Flueckiger <Lorenzo.Flueckiger@gmail.com>
2020-10-07 22:12:19 -0700

    fixed grid bug

--- src/graph_samples.h
+++ src/graph_samples.h
@@ -20,21 +20,31 @@ class GraphSamples : public DisplaySamples<kGraphWidth, int16_t> {
 
   void Draw(GFXcanvas1 *black, GFXcanvas1 *red, uint8_t background_color = 0,
             uint8_t foreground_color = 1);
+
+ protected:
+  const int16_t vStep_ = 100;
+  int16_t lowerLimit_;
+  int16_t upperLimit_;
+  int16_t ValueToVerticalPixel(int16_t value) {
+      return (kGraphYstart - kGraphHeight * (value - lowerLimit_) / (upperLimit_ - lowerLimit_));
+  }
 };
 
 void GraphSamples::Draw(GFXcanvas1 *black, GFXcanvas1 *red, uint8_t bgColor,
                         uint8_t fgColor) {
-  // draw the left/right vertical line of the graph boundary.
-  // (all the others boundaries will be draw together with the tick marks)
-  black->drawFastVLine(kGraphXstart, kGraphYstart, -kGraphHeight, fgColor);
-  black->drawFastVLine(kGraphXstart + kGraphWidth, kGraphYstart, -kGraphHeight,
-                       fgColor);
-
   // Round upper and lower limits to 100 AQI
-  const int16_t vStep = 100;
-  const int16_t lowerLimit = min_ - min_ % vStep;
-  const int16_t upperLimit = ((max_ - 1) / vStep + 1) * vStep;
-  printf("upper limit = %d\n", upperLimit);
+  lowerLimit_ = min_ - min_ % vStep_;
+  upperLimit_ = ((max_ - 1) / vStep_ + 1) * vStep_;
+
+  // Grid
+  for (int16_t h = 0; h < 5; h++) {
+    int16_t x = kGraphXstart + ((float)kGraphWidth / 4.0) * h;
+    black->drawFastVLine(x, kGraphYstart, -kGraphHeight, fgColor);
+  }
+  for (int16_t v = lowerLimit_; v <= upperLimit_; v += vStep_ / 2) {
+    int16_t y = ValueToVerticalPixel(v);
+    black->drawFastHLine(kGraphXstart, y, kGraphWidth, fgColor);
+  }
 
   // char buffer[8];
   black->setTextColor(fgColor);
@@ -62,17 +72,17 @@ void GraphSamples::Draw(GFXcanvas1 *black, GFXcanvas1 *red, uint8_t bgColor,
   // upper limit text
   int16_t uBase = EPD_HEIGHT - kGraphHeight + 16;
   black->setCursor(EPD_WIDTH - 27, uBase);
-  black->print(upperLimit / 100);
+  black->print(upperLimit_ / 100);
   // lower limit text
   int16_t lBase = EPD_HEIGHT - 2;
   bool lowIsZero = false;
-  if (lowerLimit < 100) {
+  if (lowerLimit_ < 100) {
     black->setCursor(EPD_WIDTH - 20, lBase);
     black->print("0");
     lowIsZero = true;
   } else {
     black->setCursor(EPD_WIDTH - 27, lBase);
-    black->print(lowerLimit / 100);
+    black->print(lowerLimit_ / 100);
   }
   // black->setFont(&Picopixel);
   black->setFont(&ClearSans_Medium8pt7b);
@@ -83,23 +93,12 @@ void GraphSamples::Draw(GFXcanvas1 *black, GFXcanvas1 *red, uint8_t bgColor,
   black->setCursor(EPD_WIDTH - 16, uBase - 5);
   black->print("00");
 
-  // Vertical ticks
-  int16_t y = kGraphYstart;
-  // int16_t xLabel = kGraphXstart + kGraphWidth;
-  for (int16_t v = lowerLimit; v <= upperLimit; v += vStep) {
-    black->drawFastHLine(kGraphXstart, y, kGraphWidth + 4, fgColor);
-    // sprintf(buffer, "%d", v);
-    // black->setCursor(xLabel + 6, y + 4);
-    // black->print(buffer);
-    y -= vStep;
-  }
-
   // Actually draw the Time Serie :-)
   int16_t y1 = 0;
   for (size_t i = 0; i < length_; i++) {
     int16_t data = Value(i);
-    int16_t y2 = kGraphYstart -
-                 kGraphHeight * (data - lowerLimit) / (upperLimit - lowerLimit);
+
+    int16_t y2 = ValueToVerticalPixel(data);
     if (y1 != 0 && data != INT16_MIN) {
       if (y1 < y2) {
         black->drawFastVLine(i + kGraphXstart, y1 - 1, 2 + (y2 - y1), fgColor);
