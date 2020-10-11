27346b0aca087ad5b8aa9f6b9419937861d49eae
Lorenzo Flueckiger <Lorenzo.Flueckiger@gmail.com>
2020-10-10 17:40:25 -0700

    added red densities on the graph

--- src/graph_samples.cpp
+++ src/graph_samples.cpp
@@ -14,27 +14,16 @@ int16_t GraphSamples::ValueToVerticalPixel(int16_t value) {
           kGraphHeight * (value - lowerLimit_) / (upperLimit_ - lowerLimit_));
 }
 
-void GraphSamples::Draw(GFXcanvas1 *black, GFXcanvas1 *red, uint8_t bgColor,
-                        uint8_t fgColor) {
-  // Round upper and lower limits to 100 AQI
-  lowerLimit_ = min_ - min_ % vStep_;
-  upperLimit_ = ((max_ - 1) / vStep_ + 1) * vStep_;
+// Allocate 8K of randomness
+const size_t kMaxRandomNumbers = 2048;
+uint32_t gRandoms[kMaxRandomNumbers];
 
-  // Grid
-  for (int16_t h = 0; h < 5; h++) {
-    int16_t x = kGraphXstart + ((float)kGraphWidth / 4.0) * h;
-    black->drawFastVLine(x, kGraphYstart, -kGraphHeight, fgColor);
-  }
-  for (int16_t v = lowerLimit_; v <= upperLimit_; v += vStep_ / 2) {
-    int16_t y = ValueToVerticalPixel(v);
-    black->drawFastHLine(kGraphXstart, y, kGraphWidth, fgColor);
-  }
-
+void GraphSamples::Labels() {
   // char buffer[8];
-  black->setTextColor(fgColor);
-  black->setTextSize(1);
-  black->setTextWrap(false);
-  black->setFont(&ClearSans_Medium12pt7b);
+  blackCanvas_->setTextColor(1);
+  blackCanvas_->setTextSize(1);
+  blackCanvas_->setTextWrap(false);
+  blackCanvas_->setFont(&ClearSans_Medium12pt7b);
 
   // legend
   uint32 hourPeriod = length_ * period_ / 3600;
@@ -42,52 +31,110 @@ void GraphSamples::Draw(GFXcanvas1 *black, GFXcanvas1 *red, uint8_t bgColor,
   char msg[3];
   if (hourPeriod < 100) {
     int16_t base = EPD_HEIGHT - 52;
-    black->setCursor(EPD_WIDTH - 11, base + 8);
-    black->print("h");
+    blackCanvas_->setCursor(EPD_WIDTH - 11, base + 8);
+    blackCanvas_->print("h");
     sprintf(msg, "%d", hourPeriod);
     for (size_t c = 0; c < 2; c++) {
       char l = msg[c];
-      black->setCursor(EPD_WIDTH - 22, base);
-      black->print(l);
+      blackCanvas_->setCursor(EPD_WIDTH - 22, base);
+      blackCanvas_->print(l);
       base += 16;
     }
   }
 
   // upper limit text
   int16_t uBase = EPD_HEIGHT - kGraphHeight + 16;
-  black->setCursor(EPD_WIDTH - 27, uBase);
-  black->print(upperLimit_ / 100);
+  blackCanvas_->setCursor(EPD_WIDTH - 27, uBase);
+  blackCanvas_->print(upperLimit_ / 100);
   // lower limit text
   int16_t lBase = EPD_HEIGHT - 2;
   bool lowIsZero = false;
   if (lowerLimit_ < 100) {
-    black->setCursor(EPD_WIDTH - 20, lBase);
-    black->print("0");
+    blackCanvas_->setCursor(EPD_WIDTH - 20, lBase);
+    blackCanvas_->print("0");
     lowIsZero = true;
   } else {
-    black->setCursor(EPD_WIDTH - 27, lBase);
-    black->print(lowerLimit_ / 100);
+    blackCanvas_->setCursor(EPD_WIDTH - 27, lBase);
+    blackCanvas_->print(lowerLimit_ / 100);
   }
-  // black->setFont(&Picopixel);
-  black->setFont(&ClearSans_Medium8pt7b);
+  // blackCanvas_->setFont(&Picopixel);
+  blackCanvas_->setFont(&ClearSans_Medium8pt7b);
   if (!lowIsZero) {
-    black->setCursor(EPD_WIDTH - 16, lBase - 5);
-    black->print("00");
+    blackCanvas_->setCursor(EPD_WIDTH - 16, lBase - 5);
+    blackCanvas_->print("00");
   }
-  black->setCursor(EPD_WIDTH - 16, uBase - 5);
-  black->print("00");
+  blackCanvas_->setCursor(EPD_WIDTH - 16, uBase - 5);
+  blackCanvas_->print("00");
+}
 
+void GraphSamples::Background() {
+  // Let's have some fun: I do not know how to various shade of
+  // grey with dithering, so just draw random dots with various
+  // densities! The EPS has a real random generator that can 
+  // produce plenty enough of numbers in less than 10ms
+  unsigned long start = millis();
+  ESP.random((uint8_t*)gRandoms, kMaxRandomNumbers*sizeof(uint32_t));
+  unsigned long stop = millis();
+  printf("generated %d random bytes in %ldms\n", kMaxRandomNumbers*sizeof(uint32_t),
+         stop - start);
+
+  const float densities[] = {0.15, 0.3, 0.5, 0.7, 0.7};
+  const int16_t xMin = kGraphXstart + 1;
+  const int16_t xMax = kGraphXstart + kGraphWidth - 1;
+  const int16_t xRange = xMax - xMin;
+  size_t currentDensity = 0;
+  const int16_t lowLine = (lowerLimit_ > 0) ? lowerLimit_ : 50;
+  const int16_t highLine = (upperLimit_ < 300) ? upperLimit_ : 300;
+  for (int16_t d = lowLine; d < highLine; d += 50) {
+    const int16_t yMin = ValueToVerticalPixel(d) - 1;
+    const int16_t yMax = ValueToVerticalPixel(d + 50) + 1;
+    const int16_t yRange = yMin - yMax;
+    const size_t xSeed = ESP.random() % xRange;
+    const size_t ySeed = ESP.random() % yRange;
+    const size_t nbPixels =
+        densities[currentDensity] * (float)(xRange * yRange);
+    printf("d=%d : xMin=%d, xMax=%d, yMin=%d yMax=%d, npPixel=%u\n", d, xMin,
+           xMax, yMin, yMax, nbPixels);
+    for (size_t p = 0; p < nbPixels; p++) {
+      int16_t x =
+          kGraphXstart + (int16_t)(gRandoms[(p + xSeed) % kMaxRandomNumbers] % xRange);
+      int16_t y = yMax + (int16_t)(gRandoms[(p + ySeed) % kMaxRandomNumbers] % yRange);
+      redCanvas_->writePixel(x, y, 1);
+    }
+    currentDensity++;
+  }
+}
+
+void GraphSamples::Grid() {
+  // Grid (we also "clear" the Red canvas)
+  for (int16_t h = 0; h < 5; h++) {
+    int16_t x = kGraphXstart + h * kGraphWidth / 4;
+    blackCanvas_->drawFastVLine(x, kGraphYstart, -kGraphHeight, 1);
+    redCanvas_->drawFastVLine(x, kGraphYstart, -kGraphHeight, 0);
+  }
+  for (int16_t v = lowerLimit_; v <= upperLimit_; v += vStep_ / 2) {
+    int16_t y = ValueToVerticalPixel(v);
+    blackCanvas_->drawFastHLine(kGraphXstart, y, kGraphWidth, 1);
+    redCanvas_->drawFastHLine(kGraphXstart, y, kGraphWidth, 0);
+  }
+}
+
+void GraphSamples::Serie() {
   // Actually draw the Time Serie :-)
   int16_t y1 = 0;
+  // It is actually necessary to draw twice: on the black buffer to see the curve,
+  // and on the red buffer to clear any potential red pixel there.
+  // This is due to the hardware turning red ink on always *after* the black ink.
   for (size_t i = 0; i < length_; i++) {
     int16_t data = Value(i);
-
     int16_t y2 = ValueToVerticalPixel(data);
     if (y1 != 0 && data != INT16_MIN) {
       if (y1 < y2) {
-        black->drawFastVLine(i + kGraphXstart, y1 - 1, 2 + (y2 - y1), fgColor);
+        blackCanvas_->drawFastVLine(i + kGraphXstart, y1 - 1, 2 + (y2 - y1), 1);
+        redCanvas_->drawFastVLine(i + kGraphXstart, y1 - 1, 2 + (y2 - y1), 0);
       } else {
-        black->drawFastVLine(i + kGraphXstart, y2 - 1, 2 + (y1 - y2), fgColor);
+        blackCanvas_->drawFastVLine(i + kGraphXstart, y2 - 1, 2 + (y1 - y2), 1);
+        redCanvas_->drawFastVLine(i + kGraphXstart, y2 - 1, 2 + (y1 - y2), 0);
       }
     }
     if (data == INT16_MIN) {
@@ -96,4 +143,17 @@ void GraphSamples::Draw(GFXcanvas1 *black, GFXcanvas1 *red, uint8_t bgColor,
       y1 = y2;
     }
   }
+}
+
+void GraphSamples::Draw(GFXcanvas1 *black, GFXcanvas1 *red) {
+  blackCanvas_ = black;
+  redCanvas_ = red;
+  // Round upper and lower limits to 100 AQI
+  lowerLimit_ = min_ - min_ % vStep_;
+  upperLimit_ = ((max_ - 1) / vStep_ + 1) * vStep_;
+
+  Labels();
+  Background();
+  Grid();
+  Serie();
 }
--- src/graph_samples.h
+++ src/graph_samples.h
@@ -13,14 +13,19 @@ class GraphSamples : public DisplaySamples<kGraphWidth, int16_t> {
   GraphSamples(uint32_t period_in_seconds)
       : DisplaySamples(period_in_seconds) {}
 
-  void Draw(GFXcanvas1 *black, GFXcanvas1 *red, uint8_t background_color = 0,
-            uint8_t foreground_color = 1);
+  void Draw(GFXcanvas1 *black, GFXcanvas1 *red);
 
  protected:
+  GFXcanvas1 *blackCanvas_;
+  GFXcanvas1 *redCanvas_;
   const int16_t vStep_ = 100;
   int16_t lowerLimit_;
   int16_t upperLimit_;
   int16_t ValueToVerticalPixel(int16_t value);
+  void Background();
+  void Labels();
+  void Serie();
+  void Grid();
 };
 
 #endif
